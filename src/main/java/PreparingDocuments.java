6467a1bfb925985a62e768e77e48dadadbb28e69
RomaKoks <79817815081@yandex.ru>
2016-09-15 19:31:23 +0300

    success file separation and path creation

--- src/main/java/PreparingDocuments.java
+++ src/main/java/PreparingDocuments.java
@@ -1,13 +1,15 @@
 import ru.stachek66.nlp.mystem.holding.MyStemApplicationException;
 
-import java.nio.file.Path;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
 
 
 public class PreparingDocuments {
-    public static void makeDocs(String mybookname, int authorID, int bookID, int docSize)  {
+    public static void makeDocs(String mybookname, int authorID, int bookID, int docSize) {
         ArrayList<String> fileStrings = ReadFile.toArrayList(mybookname); //read file to an arraylist of lines
         MyStemJava stemJava = new MyStemJava();
         ArrayList<String> tokens = null;
@@ -21,8 +23,12 @@ public class PreparingDocuments {
         for (int i = 0; i < tokens.size()-docSize; i += docSize) {
             numberOfDocs++;
 
-            String tempPath = "docs/" + authorID + "/" + bookID + "/";
-            Path path = Paths.get(tempPath);
+            String tempPath = "src/docs/" + authorID + "/" + bookID + "/";
+            try {
+                Files.createDirectories(Paths.get(tempPath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             String pathFileName = tempPath + authorID + "-" + bookID + "-" + numberOfDocs + ".txt";
             WriteFile.fromArrayList(pathFileName, tokens, i, docSize);
         }
