dc86ac1179ae3175ec43541d7884e6e42ffb0216
anaskras <ankrasnoshchekova@gmail.com>
2016-09-17 02:28:46 +0300

    matrices and distances

---
+++ src/main/java/Matrix/Testing.java
@@ -0,0 +1,28 @@
+package Matrix;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Created by Nastya on 16.09.2016.
+ */
+public class Testing {
+    public static void main(String[] args) {
+        Matrix myMatrix = null;
+        try {
+            myMatrix = new Matrix(new Scanner(new File("src\\main\\java\\Matrix\\test.txt")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        double[][] matr2 = myMatrix.euclideanSquareDistance();
+        Matrix dist = new Matrix(matr2);
+        System.out.print(dist.toString());
+
+
+        myMatrix.averageVector();
+        System.out.print(new Matrix(myMatrix.covariationMatrix()));
+        System.out.print( new Matrix(myMatrix.inverseCovariationMatrix()));
+        System.out.print( new Matrix(myMatrix.MahalanobisDistance()));
+    }
+}
